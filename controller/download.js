a8cb6f971adc93774e16011cf0b1600d1028e414
Riku124 <105696334+Riku124@users.noreply.github.com>
2022-07-26 11:15:03 +0900

    Update download.js

--- controller/download.js
+++ controller/download.js
@@ -3,7 +3,7 @@ const { S3, GetObjectCommand } = require('@aws-sdk/client-s3')
 module.exports = function(req, res) {
     const client = new S3()
 
-    client.send(new GetObjectCommand({ Bucket: 'cupper-hj-test', key: sample.png }))
+    client.send(new GetObjectCommand({ Bucket: '', key: sample.png }))
     .then(data => {
         res.attachment('download.png')
         data.Body.pipe(res)
